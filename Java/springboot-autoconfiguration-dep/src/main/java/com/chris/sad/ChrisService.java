0ba9921b3cd755c39855592005dfe4a5aea88cb4
chris <chrischen2018@163.com>
2020-01-02 02:28:12 +0800

    mod

---
+++ Java/springboot-autoconfiguration-dep/src/main/java/com/chris/sad/ChrisService.java
@@ -0,0 +1,12 @@
+package com.chris.sad;
+
+/**
+ * Create by Chris Chan
+ * Create on 2020/1/2 2:12
+ * Use for:
+ */
+public class ChrisService {
+    public void say() {
+        System.out.println("Haha,Dep success!");
+    }
+}
---
+++ Java/springboot-autoconfiguration-dep/src/main/java/com/chris/sad/config/ChrisServiceAutoConfiguration.java
@@ -0,0 +1,23 @@
+package com.chris.sad.config;
+
+import com.chris.assist.ChrisAutoConfigurationCondition;
+import com.chris.sad.ChrisService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Create by Chris Chan
+ * Create on 2020/1/2 2:13
+ * Use for:
+ */
+@Configuration
+@ConditionalOnClass(ChrisAutoConfigurationCondition.class)
+public class ChrisServiceAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean(ChrisAutoConfigurationCondition.class)
+    public ChrisService chrisService() {
+        return new ChrisService();
+    }
+}
