2879d63c2dd63584b87e3079db2ce1c71654c894
chris <chrischen2018@163.com>
2020-01-02 03:50:55 +0800

    mod

--- Java/springboot-autoconfiguration-test/src/main/java/com/chris/junit/api/TestApi.java
+++ Java/springboot-autoconfiguration-test/src/main/java/com/chris/junit/api/TestApi.java
@@ -1,7 +1,9 @@
 package com.chris.junit.api;
 
 import com.chris.sad.ChrisService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api")
-public class TestApi {
+public class TestApi implements ApplicationContextAware {
+    private ApplicationContext context;
     //包被排除，自定义的自动配置失效了
-    @Autowired
-    ChrisService chrisService;
+    //@Autowired
+    //ChrisService chrisService;
 
     @GetMapping("/test")
     public String test() {
-        chrisService.say();
-        return "success.";
+        if (this.context.containsBean(ChrisService.class.getName())) {
+            ChrisService chrisService = this.context.getBean(ChrisService.class);
+            chrisService.say();
+            return "Test success.";
+        }
+        return "Not found bean.";
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
 }
