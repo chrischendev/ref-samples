e83f6984e42021e8448f11515e510b8ad4875d03
chris <chrischen2018@163.com>
2020-01-02 02:25:43 +0800

    mod

---
+++ Java/springboot-autoconfiguration-dep1/src/main/java/com/chris/assist/ChrisAutoConfigurationCondition.java
@@ -0,0 +1,11 @@
+package com.chris.assist;
+
+/**
+ * Create by Chris Chan
+ * Create on 2020/1/2 2:23
+ * Use for: 这个包和类使用在测试自定义自动配置的条件
+ * 在直接依赖的包的自动配置中会用到这个条件
+ * 在测试的项目中会尝试排除
+ */
+public class ChrisAutoConfigurationCondition {
+}
