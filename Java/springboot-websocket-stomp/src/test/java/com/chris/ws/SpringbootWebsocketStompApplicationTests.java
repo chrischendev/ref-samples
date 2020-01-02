5d82ae48293197495e77be508f0d13b6597cea3a
chris <chrischen2018@163.com>
2020-01-02 21:11:02 +0800

    mod

---
+++ Java/springboot-websocket-stomp/src/test/java/com/chris/ws/SpringbootWebsocketStompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsocketStompApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
