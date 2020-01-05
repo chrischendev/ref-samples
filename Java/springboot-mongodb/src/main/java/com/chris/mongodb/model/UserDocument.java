011fe6459d92dccfe091fa1775c1006835d2149f
chris <chrischen2018@163.com>
2020-01-05 13:06:37 +0800

    mod

--- Java/springboot-mongodb/src/main/java/com/chris/mongodb/model/UserDocument.java
+++ Java/springboot-mongodb/src/main/java/com/chris/mongodb/model/UserDocument.java
@@ -3,7 +3,6 @@ package com.chris.mongodb.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
